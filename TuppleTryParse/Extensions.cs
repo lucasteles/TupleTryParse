3de8399465281d5191e8402405202e809c2a4fe1
Lucas Teles <eu@lucasteles.net>
2017-09-12 18:48:03 -0300

    naming tuples

--- TuppleTryParse/Extensions.cs
+++ TuppleTryParse/Extensions.cs
@@ -4,19 +4,19 @@ namespace TuppleTryParse
 {
     public static class Parser
     {
-        public static (bool, int) TryParseInt(string value) => (int.TryParse(value, out int result),result);
-        public static (bool, double) TryParseDouble(string value) => (double.TryParse(value, out double result),result);
-        public static (bool, long) TryParseLong(string value) => (long.TryParse(value, out long result),result);
-        public static (bool, float) TryParseFloat(string value) => (float.TryParse(value, out float result),result);
-        public static (bool, decimal) TryParseDecimal(string value) => (decimal.TryParse(value, out decimal result),result);
-        public static (bool, bool) TryParseBool(string value) => (bool.TryParse(value, out bool result),result);
-        public static (bool, byte) TryParseByte(string value) => (byte.TryParse(value, out byte result),result);
-        public static (bool, DateTime) TryParseDateTime(string value) => (DateTime.TryParse(value, out DateTime result),result);
-        public static (bool, short) TryParseShort(string value) => (short.TryParse(value, out short result),result);
-        public static (bool, sbyte) TryParseSbyte(string value) => (sbyte.TryParse(value, out sbyte result),result);
-        public static (bool, char) TryParseChar(string value) => (char.TryParse(value, out char result),result);
-        public static (bool, uint) TryParseUint(string value) => (uint.TryParse(value, out uint result),result);
-        public static (bool, ulong) TryParseUlong(string value) => (ulong.TryParse(value, out ulong result),result);
-        public static (bool, ushort) TryParseUshort(string value) => (ushort.TryParse(value, out ushort result),result);
+        public static (bool success, int value) TryParseInt(string value) => (int.TryParse(value, out int result),result);
+        public static (bool success, double value) TryParseDouble(string value) => (double.TryParse(value, out double result),result);
+        public static (bool success, long value) TryParseLong(string value) => (long.TryParse(value, out long result),result);
+        public static (bool success, float value) TryParseFloat(string value) => (float.TryParse(value, out float result),result);
+        public static (bool success, decimal value) TryParseDecimal(string value) => (decimal.TryParse(value, out decimal result),result);
+        public static (bool success, bool value) TryParseBool(string value) => (bool.TryParse(value, out bool result),result);
+        public static (bool success, byte value) TryParseByte(string value) => (byte.TryParse(value, out byte result),result);
+        public static (bool success, DateTime value) TryParseDateTime(string value) => (DateTime.TryParse(value, out DateTime result),result);
+        public static (bool success, short value) TryParseShort(string value) => (short.TryParse(value, out short result),result);
+        public static (bool success, sbyte value) TryParseSbyte(string value) => (sbyte.TryParse(value, out sbyte result),result);
+        public static (bool success, char value) TryParseChar(string value) => (char.TryParse(value, out char result),result);
+        public static (bool success, uint value) TryParseUint(string value) => (uint.TryParse(value, out uint result),result);
+        public static (bool success, ulong value) TryParseUlong(string value) => (ulong.TryParse(value, out ulong result),result);
+        public static (bool success, ushort value) TryParseUshort(string value) => (ushort.TryParse(value, out ushort result),result);
     }
 }
